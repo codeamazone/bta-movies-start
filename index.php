cd62ca9875315b402771f0b4e41adf34d2f238e0
berndengels <engels@f50.de>
2021-01-20 15:42:43 +0100

    stuff

--- index.php
+++ index.php
@@ -33,8 +33,8 @@ if(isset($_GET['controller'])) {
         // prüfe, ob es ein id-parameter gibt, wenn ja: 
         // dann $controller->$action($id)
         // wenn nicht dann ohne parameter $controller->$action()
-        if(isset($_GET['action'])) {
-            $id = $_GET['action'];
+        if(isset($_GET['id'])) {
+            $id = $_GET['id'];
             $controller->$action($id);
         } else {
             $controller->$action();
