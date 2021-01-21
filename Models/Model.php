e4d211d88c9b44d3dada891a1995f849d7d438b8
codeamazone <e.niggeme@gmail.com>
2021-01-21 12:07:12 +0100

    updated AuthorController-eva

--- Models/Model.php
+++ Models/Model.php
@@ -1,16 +1,21 @@
 <?php
 require_once 'inc/MyDB.php';
-class Model extends MyDB {
+class Model extends MyDB
+{
 
     protected $table;
 
-    public function all() {
-         $sql = "SELECT * FROM $this->table";
-         return $this->getAll($sql);
+    public function all()
+    {
+        $sql = "SELECT * FROM $this->table";
+        return $this->getAll($sql);
     }
-        
-    public function find(int $id) {
-        
+
+    public function find(int $id)
+    {
+        // Abfrage mit Platzhalter ?
+        $sql = "SELECT * FROM $this->table WHERE id = ?";
+        // Übergabe des id-Werts als numerisches Array
+        return $this->getOne($sql, [$id]);
     }
 }
-?>
