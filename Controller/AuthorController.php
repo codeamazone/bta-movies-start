2e51d6f68887f40012d6651ae2620b60ce88ab18
berndengels <engels@f50.de>
2021-01-21 15:25:39 +0100

    stuff

--- Controller/AuthorController.php
+++ Controller/AuthorController.php
@@ -28,14 +28,38 @@ class AuthorController extends Controller
     
     // zeige formular zum editiern oder neu anlegen eines datensatzes an 
     public function edit($id = null) {
-
+        if($id > 0) {
+            $data = $this->model->find($id);
+        } else {
+            $data = null;
+        }
+        require_once 'Views/Forms/author.php';
     }
 
     public function store($id = null) {
 
+        $firstname  = $_POST['firstname'];
+        $lastname   = $_POST['lastname'];
+
+        if($id > 0) {
+            // update
+            $sql = "UPDATE authors SET firstname='$firstname',lastname='$lastname' WHERE id=?";
+            $stmt = $this->model->prepare($sql);
+            $stmt->execute([$id]);
+        } else {
+            // insert
+            $sql = "INSERT INTO authors (firstname,lastname) VALUES ('$firstname','$lastname')";
+            $stmt = $this->model->prepare($sql);
+            $stmt->execute();
+        }
+
+        header("location: /authors");
     }
 
     public function delete($id) {
-            header("location: /authors");
+        $sql = "DELETE FROM authors WHERE id=?";
+        $stmt = $this->model->prepare($sql);
+        $stmt->execute([$id]);
+        header("location: /authors");
     }
 }
